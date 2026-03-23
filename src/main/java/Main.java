c4291c358fd088a878e08c44bebb4917c0f0ceee
Deadshot994 <Deadshot994>
2026-03-23 19:23:43 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,19 @@
 import java.sql.SQLOutput;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) throws Exception {
 
         Scanner scanner = new Scanner(System.in);
 
+        //Built-in commands
+        Set<String> builtins = new HashSet<>(Arrays.asList("echo", "exit", "type"));
+
         while(true) {
             System.out.print("$ ");
-
             String input = scanner.nextLine();
 
             if(input.equals("exit")) {
@@ -16,6 +21,19 @@ public class Main {
             }
             else if(input.startsWith("echo")) {
                 System.out.println(input.substring(5));
+            }
+            else if(input.startsWith("type")){
+                String[] parts = input.split(" ");
+                if(parts.length < 2) continue;
+
+                String arg = parts[1];
+
+                if(arg.equals("echo") || arg.equals("exit") || arg.equals("type")) {
+                    System.out.println(arg + " command is a shell builtin");
+                }
+                else {
+                    System.out.println(arg + ": not found");
+                }
             }
             else {
                 System.out.println(input + ": command not found");
